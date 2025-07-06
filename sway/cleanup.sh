c7896971913f7a2f8837323d29d09a4d96c7b004
xpressrazor <3086604+xpressrazor@users.noreply.github.com>
2025-07-06 14:32:31 -0600

    Added cleanup script

--- sway/cleanup.sh
+++ sway/cleanup.sh
@@ -4,3 +4,16 @@
 
 cat ./myflatpakpackages.txt | xargs flatpak --system -y uninstall --delete-data
 sudo pacman -Rns $(pacman -Qdtq)
+
+#First, for all the packages "explicitly installed", change their installation reason to "installed as a dependency":
+
+# pacman -D --asdeps $(pacman -Qqe)
+
+#Then, change the installation reason to "explicitly installed" of only the essential packages, those you do not want to remove, in order to avoid targeting them:
+
+
+# pacman -D --asexplicit base linux linux-firmware
+
+pacman -D --asdeps $(pacman -Qqe)
+pacman -D --asexplicit base linux linux-headers linux-firmware alsa-firmware alsa-utils autotiling-git blueman brightnessctl cliphist comicthumb envycontrol ffmpegthumbnailer flatpak fprintd git gparted grim gvfs kitty-git kwallet-pam lib32-pipewire lib32-vulkan-intel neovim mpv networkmanager networkmanager-applet ntfs-3g nvidia-open nvidia-prime nvidia-settings nwg-look plocate stow sway swayinteractive-screenshot swaybg swayidle swaync tlp  ttf-0xproto-nerd ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols-mono ttf-roboto-mono tumbler tumbler-extra-thumbnailers ufw unarchiver vkd3d vulkan-intel vulkan-mesa-layers vulkan-tools waybar waypaper wget wlsunset wofi xdg-desktop-portal-wlr xorg-xhost xorg-xwayland
+
