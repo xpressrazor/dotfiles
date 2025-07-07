79b42ff372f860f4380b4eb303c9a28fe06e1494
xpressrazor <3086604+xpressrazor@users.noreply.github.com>
2025-07-07 07:08:11 -0600

    Added packages

--- sway/cleanup.sh
+++ sway/cleanup.sh
@@ -2,6 +2,7 @@
 ## Uninstalls all packages installed, except important packages (need to put packages that are system required vs users)
 ## Try to limit packages that are installed by yay or pacman
 
+sudo pacman -Syu
 cat ./myflatpakpackages.txt | xargs flatpak --system -y uninstall --delete-data
 sudo pacman -Rns $(pacman -Qdtq)
 
@@ -14,6 +15,6 @@ sudo pacman -Rns $(pacman -Qdtq)
 
 # pacman -D --asexplicit base linux linux-firmware
 
-pacman -D --asdeps $(pacman -Qqe)
-pacman -D --asexplicit base linux linux-headers linux-firmware alsa-firmware alsa-utils autotiling-git blueman brightnessctl cliphist comicthumb envycontrol ffmpegthumbnailer flatpak fprintd git gparted grim gvfs kitty-git kwallet-pam lib32-pipewire lib32-vulkan-intel neovim mpv networkmanager networkmanager-applet ntfs-3g nvidia-open nvidia-prime nvidia-settings nwg-look plocate stow sway swayinteractive-screenshot swaybg swayidle swaync tlp  ttf-0xproto-nerd ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols-mono ttf-roboto-mono tumbler tumbler-extra-thumbnailers ufw unarchiver vkd3d vulkan-intel vulkan-mesa-layers vulkan-tools waybar waypaper wget wlsunset wofi xdg-desktop-portal-wlr xorg-xhost xorg-xwayland
+sudo pacman -D --asdeps $(pacman -Qqe)
+sudo pacman -D --asexplicit base linux linux-headers linux-firmware alsa-firmware alsa-utils autotiling-git blueman brightnessctl cliphist comicthumb envycontrol ffmpegthumbnailer flatpak fprintd git gparted grim gvfs kitty-git kwallet-pam lib32-pipewire lib32-vulkan-intel neovim mpv networkmanager networkmanager-applet ntfs-3g nvidia-open nvidia-prime nvidia-settings nwg-look plocate stow sway swayinteractive-screenshot swaybg swayidle swaync tlp  ttf-0xproto-nerd ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols-mono ttf-roboto-mono tumbler tumbler-extra-thumbnailers ufw unarchiver vkd3d vulkan-intel vulkan-mesa-layers vulkan-tools waybar waypaper wget wlsunset wofi xdg-desktop-portal-wlr xorg-xhost xorg-xwayland
 
