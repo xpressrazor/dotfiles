09e602c62efe9a0fcb89759a8397c2e2f1bec588
xpressrazor <3086604+xpressrazor@users.noreply.github.com>
2025-09-18 11:15:40 -0600

    Add opensingle.py command

---
+++ bin/.local/bin/opensingle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import sys
+import subprocess
+import os
+
+if len(sys.argv) >= 3:
+    
+    workspaceswitchcmd = ['hyprctl', 'dispatch', 'workspace', sys.argv[2]]
+    # Switch workspace first
+    subprocess.Popen(workspaceswitchcmd)
+
+    workspacenumbercd = ""
+    workspaceid = ""
+   
+    # If workspace provided, match workspace as well
+    if len(sys.argv) == 4:
+        workspaceidcmd = "hyprctl clients -j | jq -r \'.[] | select(.class == \"" + sys.argv[3] + "\") | .workspace.id\'"
+        os.system(workspaceidcmd)
+        workspaceid = os.popen(workspaceidcmd).read();
+
+    if sys.argv[1].count(".") == 2:
+        pscmd = 'flatpak ps | grep ' + sys.argv[1];
+        os.system(pscmd)
+        result = os.popen(pscmd).read()
+
+        if len(result) == 0 or workspaceid.count(sys.argv[2]) == 0:
+            subprocess.Popen(['flatpak', 'run', sys.argv[1]])
+    else:
+        hyprcmd = 'hyprctl clients | grep -i class | grep ' + sys.argv[1]
+        os.system(hyprcmd)
+        result = os.popen(hyprcmd).read()
+        
+        if len(result) == 0 or workspaceid.count(sys.argv[2]) == 0:
+            subprocess.Popen(sys.argv[1])
+else:
+    print("Invalid arguments: opensingle.py <app_name> <workspace_number <class>")
+    print("E.g. opensingle.py com.google.Chrome 3 google-chrome")
+    print("E.g. opensingle.py thunar 5")
+    print("E.g. opensingle.py thunar 5 thunar")
+
